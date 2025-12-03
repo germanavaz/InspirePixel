fcb25ba8545cf39da0074d9ff106f83870a9098d
germanavaz <gmsvaz@gmail.com>
2025-12-03 15:50:31 -0300

    :sparkles: feat: seção inspire-se

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Banner from "./components/Banner.vue";
+import Inspire from './components/Inspire.vue';
 </script>
 
 <template>
 <Header />
 <Banner />
+<Inspire />
 </template>
 
 <style>
